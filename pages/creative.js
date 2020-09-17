d1d1797eebe4bfa58b4eac889b9365ec42cb9eb8
Iman345 <60913360+Iman345@users.noreply.github.com>
2020-09-17 16:17:19 +0200

    Update creative.js

--- pages/creative.js
+++ pages/creative.js
@@ -1,2 +1,26 @@
 
-console.log(d3);
+
+let vizArea=d3.select('#Viz');
+let xScale=d3.scaleLinear().domain([0,100]).range([0,500]);
+console.log(xScale(5)+'px');
+
+vizArea
+.append("circle")
+.attr("cx", xScale(10))
+.attr("cy", 100)
+.attr("r", 40)
+.style('fill', "pink");
+
+vizArea
+.append("circle")
+.attr("cx", xScale(50))
+.attr("cy", 100)
+.attr("r", 40)
+.style('fill', "pink");
+
+vizArea
+.append("circle")
+.attr("cx", xScale(75))
+.attr("cy", 100)
+.attr("r", 40)
+.style('fill', "pink");
