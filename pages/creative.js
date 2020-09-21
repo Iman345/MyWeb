1e97e636456c915356c07bbc9e1970a1a880baa1
Iman345 <60913360+Iman345@users.noreply.github.com>
2020-09-21 19:11:13 +0200

    Update creative.js

--- pages/creative.js
+++ pages/creative.js
@@ -160,3 +160,70 @@ async function getBpi(){
     console.log(bpi);
 }
 getBpi();
+
+function parseData(coindata){
+    var arr=[];
+    for (var i in coindata.bpi ){
+        arr.push({ date : new Date(i),
+            value: +data.bpi[i]
+
+        });
+    }
+    return arr
+}
+
+function linegraph(coindata){
+   
+ var margin={top:20, right:20, bottom:30, left:50};
+
+var Width=600;
+
+var Height=600;
+
+var graphwidth = Width-margin.left-margin.right;
+var graphheight = Height- margin.top-margin.bottom;
+
+var vizArea=d3.select('#Viz');
+
+vizArea
+.attr("width", Width)
+.attr("height", Height);
+vizArea
+.append(g)
+.attr("transform", "translate("+ margin.left+","+margin.top+")");
+
+var xScale=d3.scaleTime().rangeRound([0, graphwidth]);
+var yScale=d3.scaleTime().rangeRound([graphheight, 0]);
+
+
+let drawlines=d3.line()
+.xScale(function(d){return xScale(d.date)})
+.yScale(function(d){return yScale(d.value)})
+xScale.domain(d3.extent(coindata, function(d){return d.date}));
+yScale.domain(d3.extent(coindata, function(d){return d.value}));
+
+g.append("g")
+.attr("transform", "translate(0,"+ graphheight+")")
+.call(d3.axisBotton(xScale))
+.select(".domain")
+.remove();
+
+g.append("g")
+.call(d3.axisLeft(yScale))
+.append("text")
+.attr("fill", "#000")
+.attr("y", 6)
+.attr("dy", "0.71em")
+.attr("text-anchor", "end")
+.text("Price (Dollars)");
+
+
+g.append("path")
+.datum(data)
+.attr("stroke", "pink")
+.attr ("fill", "none")
+.attr("stroke-lingejoin","round")
+.attr("stroke-linecap", "round")
+.attr("stroke-width", 2)
+.attr("d", line)
+}
