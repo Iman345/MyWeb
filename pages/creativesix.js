9c48240129b326c2323cd35a27afddbd26e6c15e
Iman345 <iman345200@gmail.com>
2020-11-25 03:18:04 -1200

    updates push

--- pages/creativesix.js
+++ pages/creativesix.js
@@ -1,2 +1,20 @@
 
 
+fetch('https://api.covid19tracking.narrativa.com/api/2020-03-22/country/southafrica')
+.then(function(response) {
+  console.log(`status: ${response.status}`);
+  console.dir(response.body);
+  return response.json() 
+})
+.then(function(myJson) {
+ console.log(
+    `Date: ${myJson.total.date} 
+    Total Deaths: ${myJson.total.today_deaths} 
+    Recovered: ${myJson.total.today_new_recovered} 
+    New Cases: ${myJson.total.today_new_confirmed}`
+  );
+  let dates=myJson.total.date;
+  return myJson
+})
+.then(console.log(dates))
+
